778c107e0a624514a24bf107b4d0d079dc0e5335
CallitMez <lhekkers054@gmail.com>
2016-10-21 20:26:36 +0200

    Added summary
    We should start doing this for all the classes

--- RPG/Button.cs
+++ RPG/Button.cs
@@ -1,11 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Microsoft.Xna.Framework;
+﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
-using Microsoft.Xna.Framework.Input;
+
+
+/// <summary>
+/// -----------------------------------------------
+/// -------------------Button.cs-------------------
+/// ---------------Made by: CallitMez--------------
+/// ----------------Made for RPGGame---------------
+/// -------------------12-10-2016------------------
+/// ------------github.com/CallitMez/RPG-----------
+/// -----------------------------------------------
+/// </summary>
 
 namespace RPG
 {
